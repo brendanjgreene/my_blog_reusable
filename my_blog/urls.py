e9fe7120b4b71f40ccbe0166711a35557c5d96c4
Brendan Greene <brendanjgreene@gmail.com>
2017-04-15 23:38:41 +0100

    hide edit button if not user

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -4,6 +4,7 @@ import views
 urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
     url(r'^(?P<id>\d+)/$', views.post_detail),
+    url(r'^top/(?P<id>\d+)/$', views.post_detail),
     url(r'^top/$', views.top_lists),
     url(r'^post/new/$', views.new_post, name='new_post'),
     url(r'^(?P<id>\d+)/edit$', views.edit_post, name="edit_post"),
